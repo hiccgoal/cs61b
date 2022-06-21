15cdd6d74f11ed995a1a1a386467d7900eedc4c4
hiccgoal <jhawang@163.com>
2022-06-21 17:08:53 +0800

    1 completed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -1,4 +1,5 @@
 import java.util.Formatter;
+import java.util.stream.StreamSupport;
 
 /**
  * A naked recursive list of integers, similar to what we saw in lecture 3, but
@@ -82,7 +83,17 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr = A;
+        if(ptr == null) {
+            return B;
+        }
+        else {
+            while (ptr.rest != null) {
+                ptr = ptr.rest;
+            }
+            ptr.rest = B;
+        }
+        return A;
     }
 
     /**
@@ -91,14 +102,29 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null) {
+            return B;
+        }
+        else if(B == null) {
+            return A;
+        }
+        else{
+            IntList res = new IntList(A.first, null);
+            IntList ptr = res;
+            A = A.rest;
+            while(A != null) {
+                ptr.rest = new IntList(A.first,null);
+                A = A.rest;
+                ptr = ptr.rest;
+            }
+            while(B != null) {
+                ptr.rest = new IntList(B.first, null);
+                B = B.rest;
+                ptr = ptr.rest;
+            }
+            return res;
+        }
     }
-
-
-
-
-
-
 
 
 
