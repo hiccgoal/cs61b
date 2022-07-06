fa3ad99656505233c0cb13ea0af674303dca44ae
hiccgoal <jhawang@163.com>
2022-07-06 17:49:14 +0800

    completed proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -2,54 +2,55 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDequeGold {
-    /** 测试次数 */
-    private int testNum = 500;
-
-
     @Test
-    public void test() {
-        StudentArrayDeque<Integer> a = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> b = new ArrayDequeSolution<>();
-        StringBuilder msg = new StringBuilder();
-
-        int elemNum = 0;
-        for (int i = 0; i < testNum; i++) {
-            double randomNum = StdRandom.uniform();
-            if (randomNum < 0.25) {
-                a.addFirst(i);
-                b.addFirst(i);
-                elemNum ++;
-                msg.append("addFirst(" + i + ")\n");
-//                assertEquals(msg.toString(), b.get(0), a.get(0));
-            } else if (randomNum < 0.5) {
-                a.addLast(i);
-                b.addLast(i);
-                elemNum ++;
-                msg.append("addLast(" + i + ")\n");
-//                assertEquals(msg.toString(), b.get(elemNum - 1), a.get(elemNum - 1));
-            } else if (randomNum < 0.75) {
-                if (b.isEmpty()) {
-                    msg.append("isEmpty()\n");
-                    assertTrue(msg.toString(), a.isEmpty());
-                    continue;
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> testArray = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> stdArray = new ArrayDequeSolution<>();
+        String log = "";
+        for (int i = 0; i < 1000; i++) {
+            if (stdArray.size() == 0) {
+                int addNumber = StdRandom.uniform(1000);
+                int headOrBack = StdRandom.uniform(2);
+                if (headOrBack == 0) {
+                    log = log + "addFirst(" + addNumber + ")\n";
+                    testArray.addFirst(addNumber);
+                    stdArray.addFirst(addNumber);
+                } else {
+                    log = log + "addLast(" + addNumber + ")\n";
+                    testArray.addLast(addNumber);
+                    stdArray.addLast(addNumber);
                 }
-                Integer A = a.removeFirst();
-                Integer B = b.removeFirst();
-                elemNum --;
-                msg.append("RemoveFirst()\n");
-                assertEquals(msg.toString(), B, A);
             } else {
-                if (b.isEmpty()) {
-                    msg.append("isEmpty()\n");
-                    assertTrue(msg.toString(), a.isEmpty());
-                    continue;
+                int x = StdRandom.uniform(4);
+                int addNumber = StdRandom.uniform(1000);
+                Integer testremoveNumber = 1;
+                Integer stdremoveNumber = 1;
+                switch (x) {
+                    case 0:
+                        log = log + "addFirst(" + addNumber + ")\n";
+                        testArray.addFirst(addNumber);
+                        stdArray.addFirst(addNumber);
+                        break;
+                    case 1:
+                        log = log + "addLast(" + addNumber + ")\n";
+                        testArray.addLast(addNumber);
+                        stdArray.addLast(addNumber);
+                        break;
+                    case 2:
+                        log = log + "removeFirst()\n";
+                        testremoveNumber = testArray.removeFirst();
+                        stdremoveNumber = stdArray.removeFirst();
+                        break;
+                    case 3:
+                        log = log + "removeLast()\n";
+                        testremoveNumber = testArray.removeLast();
+                        stdremoveNumber = stdArray.removeLast();
+                        break;
+                    default:
                 }
-                Integer A = a.removeLast();
-                Integer B = b.removeLast();
-                elemNum --;
-                msg.append("RemoveLast()\n");
-                assertEquals(msg.toString(), B, A);
+                assertEquals(log, stdremoveNumber, testremoveNumber);
             }
         }
+
     }
 }
